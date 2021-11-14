85e819dd223ba4092dff6e7402cbf9d7cb7af1ca
Heberth Reinstein <49157694+heberthreinstein@users.noreply.github.com>
2021-11-14 16:59:12 -0300

    configbuttons

--- configbuttons.py
+++ configbuttons.py
@@ -1,6 +1,16 @@
 import time
 import vgamepad as vg
 
+BTN_DELIV_INC = vg.XUSB_BUTTON.XUSB_GAMEPAD_A
+BTN_DELIV_DEC = vg.XUSB_BUTTON.XUSB_GAMEPAD_B
+BTN_RECOV_INC = vg.XUSB_BUTTON.XUSB_GAMEPAD_X
+BTN_RECOV_DEC = vg.XUSB_BUTTON.XUSB_GAMEPAD_Y
+BTN_MGU_H = vg.XUSB_BUTTON.XUSB_GAMEPAD_BACK
+BTN_EB_INC = vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_SHOULDER
+BTN_EB_DEC = vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_THUMB
+BTN_BB_FWRD = vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_SHOULDER
+BTN_BB_REAR = vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_THUMB
+
 gamepad = vg.VX360Gamepad()
 
 def pressButton(button):
@@ -11,8 +21,38 @@ def pressButton(button):
     gamepad.update()
     time.sleep(0.1)
 
-#Press a button to incialize emulator
-pressButton(vg.XUSB_BUTTON.XUSB_GAMEPAD_START)
-time.sleep(5)
-#button to config
 pressButton(vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_THUMB)
+
+countToTen():
+    for i in range(10): 
+        print(i)
+        time.sleep(1)
+
+#Press a button to incialize emulator
+print("Delivery increse button will be pressed in 10seconds")
+countToTen()
+pressButton(BTN_DELIV_INC)
+print("Delivery decrease button will be pressed in 10seconds")
+countToTen()
+pressButton(BTN_DELIV_DEC)
+print("Recovery increse button will be pressed in 10seconds")
+countToTen()
+pressButton(BTN_RECOV_INC)
+print("Recovery decrease button will be pressed in 10seconds")
+countToTen()
+pressButton(BTN_RECOV_DEC)
+print("MGU H button will be pressed in 10seconds")
+countToTen()
+pressButton(BTN_MGU_H)
+print("Engine Brake increse button will be pressed in 10seconds")
+countToTen()
+pressButton(BTN_EB_INC)
+print("Engine Brake decrease button will be pressed in 10seconds")
+countToTen()
+pressButton(BTN_EB_DEC)
+print("Break bias foward button will be pressed in 10seconds")
+countToTen()
+pressButton(BTN_BB_FWRD)
+print("Break bias backword button will be pressed in 10seconds")
+countToTen()
+pressButton(BTN_BB_REAR)
