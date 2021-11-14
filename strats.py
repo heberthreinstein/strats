a5c7af85a66a7be3e50ab4fb1452bcd244defc3c
Heberth Reinstein <heberth.reinstein@gmaile.com>
2021-11-14 12:51:24 -0300

    break balance and more strats

--- strats.py
+++ strats.py
@@ -31,9 +31,11 @@ BTN_DELIV_INC = vg.XUSB_BUTTON.XUSB_GAMEPAD_A
 BTN_DELIV_DEC = vg.XUSB_BUTTON.XUSB_GAMEPAD_B
 BTN_RECOV_INC = vg.XUSB_BUTTON.XUSB_GAMEPAD_X
 BTN_RECOV_DEC = vg.XUSB_BUTTON.XUSB_GAMEPAD_Y
-BTN_MGU_H = vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_THUMB
-BTN_EB_INC = vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_UP
-BTN_EB_DEC = vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_DOWN
+BTN_MGU_H = vg.XUSB_BUTTON.XUSB_GAMEPAD_BACK
+BTN_EB_INC = vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_SHOULDER
+BTN_EB_DEC = vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_THUMB
+BTN_BB_FWRD = vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_SHOULDER
+BTN_BB_REAR = vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_THUMB
 
 
 #SETUP
@@ -41,12 +43,14 @@ SETUP_ERS_MODE = ERS_CHARGING
 SETUP_ERS_RECOVERY = 9
 SETUP_ENGINE_BRAKE = 8
 SETUP_MGUH = MGU_H_BATERY
+SETUP_BREAK_BALANCE = 53
 
 #GLOBALS
 currentERSMode = SETUP_ERS_MODE
 currentERSRecovery = SETUP_ERS_RECOVERY
 currentEngineBraking = SETUP_ENGINE_BRAKE
 currentMGUH = SETUP_MGUH
+currentBreakBalance = SETUP_BREAK_BALANCE
 
 def reset():
     global currentERSMode
@@ -57,6 +61,8 @@ def reset():
     currentEngineBraking = SETUP_ENGINE_BRAKE
     global currentMGUH
     currentMGUH = SETUP_MGUH
+    global currentBreakBalance
+    currentBreakBalance = SETUP_BREAK_BALANCE
     print('RESET')
 
 def changeERSMode(mode):
@@ -79,12 +85,12 @@ def changeRecovery(n):
     diff = n - currentERSRecovery
     if (diff > 0): 
         for _ in range(diff):
-            pressButton(BTN_DELIV_INC)
+            pressButton(BTN_RECOV_INC)
         currentERSRecovery = n
 
     if (diff < 0):
         for _ in range(abs(diff)): 
-            pressButton(BTN_DELIV_DEC)
+            pressButton(BTN_RECOV_DEC)
         currentERSRecovery = n
 
 def changeEngineBraking(n):
@@ -100,6 +106,22 @@ def changeEngineBraking(n):
         for _ in range(abs(diff)): 
             pressButton(BTN_EB_DEC)
         currentEngineBraking = n
+
+def changeBreakBalance(n):
+    print("Changing Break Balance =>", n)
+    global currentBreakBalance
+    diff = n - currentBreakBalance
+    if (diff > 0): 
+        for _ in range(diff):
+            pressButton(BTN_BB_FWRD)
+            pressButton(BTN_BB_FWRD)
+        currentBreakBalance = n
+
+    if (diff < 0):
+        for _ in range(abs(diff)): 
+            pressButton(BTN_BB_REAR)
+            pressButton(BTN_BB_REAR)
+        currentBreakBalance = n
 
 def changeMGUH(mode):
     print("Changing MGUH =>", mode)
@@ -123,17 +145,50 @@ def setup():
 def qualifying():
     changeERSMode(ERS_HOTLAP)
     changeMGUH(MGU_H_MOTOR)
-    changeRecovery(SETUP_ERS_RECOVERY)
+    changeRecovery(0)
     changeEngineBraking(SETUP_ENGINE_BRAKE)
+
+def stratOne():
+    changeEngineBraking(11)
+    changeBreakBalance(53)
+
+def stratTwo():
+    changeEngineBraking(8)
+    changeBreakBalance(55)
+
+def push():
+    changeRecovery(4)
+    changeERSMode(ERS_BALANCED_HIGH)
+
+def saveBattery():
+    changeERSMode(ERS_BALANCED_LOW)
+    changeRecovery(10)
+    changeEngineBraking(10)
+    changeBreakBalance(57)
+    changeMGUH(MGU_H_BATERY)
+
+def fullPush():
+    changeERSMode(ERS_OVERTAKE)
+    changeRecovery(4)
+    changeMGUH(MGU_H_MOTOR)
+
+def sendIT():
+    changeERSMode(ERS_TOP_SPEED)
+    changeEngineBraking(SETUP_ENGINE_BRAKE)
+    changeMGUH(MGU_H_MOTOR)
+    changeRecovery(0)
+
 
 while True:
     keyboard.add_hotkey('r', recharge)
-    keyboard.add_hotkey('c', changeERSMode, [ERS_BALANCED_LOW])
+    keyboard.add_hotkey('c', fullPush)
     keyboard.add_hotkey('v', changeERSMode, [ERS_BALANCED_HIGH])
-    keyboard.add_hotkey('z', changeERSMode, [ERS_OVERTAKE])
-    keyboard.add_hotkey('x', changeERSMode, [ERS_TOP_SPEED])
+    keyboard.add_hotkey('z', push)
+    keyboard.add_hotkey('x', saveBattery)
     keyboard.add_hotkey('q', qualifying)
     keyboard.add_hotkey('s', setup)
+    keyboard.add_hotkey('1', stratOne)
+    keyboard.add_hotkey('2', stratTwo)
     keyboard.add_hotkey('0', reset)
 
     keyboard.wait()
